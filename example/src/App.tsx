2cafa41a0ca9727902249f98d9c1c0bb3384c1ae
Eric Thomas <bluebird6922@gmail.com>
2022-06-08 10:26:48 +0300

    add example

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SayHello from 'acctez-main';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name="Prateek" />
+    </div>
+  );
+}
+
+export default App;
