8c3f4cebb4bd541b10de7013aafc30ddd9737ef1
Eric Thomas <bluebird6922@gmail.com>
2022-06-17 10:35:47 +0300

    fix config error

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HelloWorld, useWallet } from 'acctez-main';
+import { useWallet } from 'acctez-main';
 import './App.css';
 
 
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld name="Eric" />
         <button onClick={() => connectWallet()}>Connect Wallet</button>
       </header>
     </div>
